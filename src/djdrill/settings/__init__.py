cde2e1de98ca0c5c20015cfdcf6d3bb3843cd6d7
Ankit <ankit@Leoankit.(none)>
2013-04-06 16:09:08 +0530

    Created local settings file.

--- src/djdrill/settings/__init__.py
+++ src/djdrill/settings/__init__.py
@@ -5,6 +5,10 @@ hostname = socket.gethostname()
 
 if hostname == "arun-desktop":
     from settings_arun import *
+elif hostname == "Leoankit":
+    from settings_ankit import *
+#elif hostname == "leo-desktop": 
+#    from  import *
 elif hostname == "Leonilesh":
     from settings_nilesh import *
 else:
